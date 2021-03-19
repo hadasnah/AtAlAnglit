8bf2b0919bd7a1adee4bea88e99307c867f8316a
hadasnah <72435482+hadasnah@users.noreply.github.com>
2021-03-19 12:06:14 +0200

    Fixed parentheses bug, updated version.

--- popup.js
+++ popup.js
@@ -1,38 +1,39 @@
 document.getElementById("translateButton").addEventListener("click", switchE2H);
 
-String.prototype.replaceAt = function(index, replacement) {
-    return this.substr(0, index) + replacement + this.substr(index + replacement.length);
+String.prototype.replaceAt = function (index, replacement) {
+	return this.substr(0, index) + replacement + this.substr(index + replacement.length);
 };
 
 function switchE2H() {
 	var inputText = document.getElementById("inputText").value;
 
 	var test = {
-		"q": "/", "w": "\"","'":",", 
-		"e": "ק", "r": "ר", "t": "א", "y": "ט", "u": "ו", "i": "ן", "o": "ם", "p": "פ", "[":"]", "]":"[",
-		"a": "ש", "s": "ד", "d": "ג", "f": "כ", "g": "ע", "h": "י", "j": "ח", "k": "ל", "l": "ך", ";":"ף", "'":",",
-		"z": "ז", "x": "ס", "c": "ב", "v": "ה",	"b": "נ", "n": "מ", "m": "צ", ",": "ת", ".":"ץ", "/": ".", "(":")",
-		")":"(","`":";"};
+		"q": "/", "w": "\"", "'": ",",
+		"e": "ק", "r": "ר", "t": "א", "y": "ט", "u": "ו", "i": "ן", "o": "ם", "p": "פ", "[": "]", "]": "[",
+		"a": "ש", "s": "ד", "d": "ג", "f": "כ", "g": "ע", "h": "י", "j": "ח", "k": "ל", "l": "ך", ";": "ף", "'": ",",
+		"z": "ז", "x": "ס", "c": "ב", "v": "ה", "b": "נ", "n": "מ", "m": "צ", ",": "ת", ".": "ץ", "/": ".", "(": ")",
+		")": "(", "`": ";"
+	};
 
-	var outputWord="";
-	for (var i = 0; i < inputText.length; i++){
+	var outputWord = "";
+	for (var i = 0; i < inputText.length; i++) {
 		var letter = inputText.charAt(i).toLowerCase();
-		if (test.hasOwnProperty(letter)){
+		if (test.hasOwnProperty(letter)) {
 			outputWord += test[letter];
 		}
-		else{
+		else {
 			outputWord += letter;
 		}
-		
+
 	}
-	
+
 	document.getElementById("result").innerHTML = outputWord;
-	
-	document.addEventListener('copy', function(e) {
-	  e.clipboardData.setData('text/plain', outputWord);
-	  e.preventDefault();
+
+	document.addEventListener('copy', function (e) {
+		e.clipboardData.setData('text/plain', outputWord);
+		e.preventDefault();
 	});
-	
+
 	document.execCommand('copy');
-	
+
 }
